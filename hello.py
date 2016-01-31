46e9f9a560c2ae82d5e6482292ce37d28419a5e9
Chen Deshan <chendeshan@chendeshandeMacBook-Pro.local>
2016-02-01 02:40:34 +0800

    kljk

---
+++ hello.py
@@ -0,0 +1,11 @@
+from flask import Flask, url_for, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+	return render_template ('te.html')
+
+if __name__ == '__main__':
+	app.run('0.0.0.0', debug=True)
+
+	
